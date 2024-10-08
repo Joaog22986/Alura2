95a25a5e7796e9d10cef5caa96d12a8974c788f5
Joaog22986 <joaogmdc22@gmail.com>
2024-10-08 09:33:01 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'política',
+    'Quantos presidentes ja teve no Brasil?',
+    '38'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital de Espirito Santo',
+    'Vitoria'
+)
+
+criaCartao(
+    'ATUALIDADES',
+    'Quem é o pai do Naruto?',
+    'Minato'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu vou a praia em inglês?',
+    'I am going to the beach'
+)
